97458f1d56ec4b4f08ea8c97b470f74a7bfed732
IHowe <yhou1@berkeley.edu>
2024-04-29 19:17:42 -0700

    Added comments for main

--- README.md
+++ README.md
@@ -9,6 +9,6 @@ time_search: Input a time range, and all messages sent within that range will be
 new_chat: Input channel ID, and all messages sent in that server within the last 7 days will be returned 
 
 ## Contributing 
-Pull requests are welcome. FOr major changes, please open an issue first to discuss what you would like to change
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change
 
 Please maek sure to update tests as appropriate
--- main.py
+++ main.py
@@ -14,16 +14,29 @@ def root():
 
 @app.get("/term_search")
 def term_search(keyword):
+    """
+    term_search takes in keyword input, returning all messages that contains the keyword
+    
+    :return: Return a list of JSON
+    """
     chats = search.by_keyword(keyword)
     return chats
 
 @app.get("/date_search")
 def time_search(start_date, end_date):
+    """
+    time_search takes in a start and end date, returning all messages sent within that time range
+    
+    :return: Return a list of JSON"""
     chats = search.by_date(start_date, end_date)
     return chats
 
 @app.post("/update_chat")
 def get_new_chat(channel_id):
+    """
+    get_new_chat takes in a channel ID, inserting all messages sent in the channel 
+    within the last 7 days into the database
+    """
     isSuccess = update_database(channel_id)
     if not isSuccess:
         return "Failed"
